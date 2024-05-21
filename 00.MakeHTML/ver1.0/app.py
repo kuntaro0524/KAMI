cff33927707687bbc0f8ea1ae649da220cfe5d78
kuntaro <kuntaro0524@gmail.com>
2024-05-22 08:44:31 +0900

    ver1.0 successfully displayed the thumbnails.

---
+++ 00.MakeHTML/ver1.0/app.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, url_for, send_from_directory, jsonify
+import os
+import json
+
+app = Flask(__name__)
+
+# JSONファイルの読み込み
+with open('sample.json') as f:
+    plates = json.load(f)
+
+# 静的ファイルのディレクトリを設定
+app.config['IMAGE_FOLDER'] = '/Volumes/kunssd01/Toma3/240508-rockimages'
+
+@app.route('/')
+def index():
+    return render_template('index.html', plates=plates)
+
+def get_latest_batch_folder(base_paths):
+    print("get_latest_batch_folder")
+    print(base_paths)
+    iBaseMax = 0
+    latest_path = ""
+    for base_path in base_paths:
+        # base_pathが"plateID_"を含まなければスキップする
+        if not 'batchID_' in base_path:
+            continue
+        # baseIDを取得する
+        batch_id = int(base_path.split('_')[1])
+        if iBaseMax < batch_id: 
+            iBaseMax = batch_id
+            latest_path = base_path
+        
+    return latest_path
+
+@app.route('/plate/<int:plate_id>')
+def plate(plate_id):
+    plate_folder = f"{str(plate_id).zfill(3)}"
+    base_path = os.path.join(app.config['IMAGE_FOLDER'], plate_folder, f"plateID_{plate_folder}")
+    batch_folders = os.listdir(base_path)
+
+    print(batch_folders)
+
+    latest_batch_folder = get_latest_batch_folder(batch_folders)
+
+    thumbnails = []
+    well_folder_path = os.path.join(base_path, latest_batch_folder)
+    debug_info = {
+        'plate_folder': plate_folder,
+        'base_path': base_path,
+        'batch_folders': batch_folders,
+        'latest_batch_folder': latest_batch_folder,
+        'well_folder_path': well_folder_path
+    }
+
+    for well_folder in os.listdir(well_folder_path):
+        well_num = int(well_folder.split('_')[1])
+        profile_folder_path = os.path.join(well_folder_path, well_folder, 'profileID_1')
+        if os.path.exists(profile_folder_path):
+            for file in os.listdir(profile_folder_path):
+                if 'th' in file and 'low' not in file and file.startswith('d1'):
+                    img_url = url_for('static_image', filename=os.path.join(plate_folder, f"plateID_{plate_folder}", latest_batch_folder, well_folder, 'profileID_1', file))
+                    thumbnails.append((well_num, img_url))
+                    break
+    thumbnails.sort(key=lambda x: x[0])
+    return render_template('plate.html', plate_id=plate_id, thumbnails=thumbnails, debug_info=debug_info)
+
+@app.route('/images/<path:filename>')
+def static_image(filename):
+    return send_from_directory(app.config['IMAGE_FOLDER'], filename)
+
+if __name__ == '__main__':
+    app.run(debug=True,port=8888)
